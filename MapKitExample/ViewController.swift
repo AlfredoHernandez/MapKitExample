6e310f27ec06417604e3087ace9785ba382047da
Alfredo Hernandez <alfredo.h.alarcon@icloud.com>
2018-06-24 13:57:16 -0500

    Initial Commit

---
+++ MapKitExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapKitExample
+//
+//  Created by Jesús Hernández Alarcón on 6/24/18.
+//  Copyright © 2018 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
